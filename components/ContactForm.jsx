f3dc448590e0a628d2ded59795a0f04e03d7623d
lee14k <kmkovach@mtu.edu>
2024-05-16 10:00:09 -0400

    contact and some service copy

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -19,10 +19,10 @@ export default function ContactForm() {
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div className=" gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              First name
+              Name
             </label>
             <div className="mt-2.5">
               <input
@@ -30,20 +30,6 @@ export default function ContactForm() {
                 name="first-name"
                 id="first-name"
                 autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
--- components/ServicesTripleBox.jsx
+++ components/ServicesTripleBox.jsx
@@ -11,7 +11,7 @@ const ServicesTripleBox =()=> {
                     alt="Landscaping"
                     width={300}
                     height={300}/>
-                <h2>Service 1</h2>
+                <h2>Tree Planting</h2>
                 <p>Service 1 description</p>
             </div>
             <div className="services-triple-box-item">
@@ -20,7 +20,7 @@ const ServicesTripleBox =()=> {
                     alt="Landscaping"
                     width={300}
                     height={300}/>
-                <h2>Service 2</h2>
+                <h2>Landscaping</h2>
                 <p>Service 2 description</p>
             </div>
             <div className="services-triple-box-item">
@@ -29,7 +29,7 @@ const ServicesTripleBox =()=> {
                     alt="Landscaping"
                     width={300}
                     height={300}/>
-                <h2>Service 3</h2>
+                <h2>Home Repair</h2>
                 <p>Service 3 description</p>
             </div>
             </div>
