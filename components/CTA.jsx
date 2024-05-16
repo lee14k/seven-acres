91a937c01965461d3e7e08852759d57b6e5525a8
lee14k <kmkovach@mtu.edu>
2024-05-16 09:56:58 -0400

    button and new pics

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 export default function CTA () {
 return (
@@ -6,8 +7,13 @@ return (
 <div className="flex flex-col justify-center items-center flower-bg text-white">
    
   <h1 className="text-8xl my-4">Looking for flowers?</h1>
-  <p className="text-4xl">Spring has sprung. Come visit us in Hyde and see our gorgeous blooms</p>
-  <button>Learn More</button>
-</div>
+  <p className="text-4xl my-4">Spring has sprung. Come visit us in Hyde and see our gorgeous blooms</p>
+  <Link href='/flowers-plants'>
+  <button
+        type="button"
+        className="rounded-md bg-sky-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Learn more
+      </button></Link></div>
 )
 }
