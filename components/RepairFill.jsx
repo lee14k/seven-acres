aad8ba36b39999f20510950e0200ed6aac3f05a1
lee14k <kmkovach@mtu.edu>
2024-05-18 09:23:17 -0400

    fixing more stuff

--- components/RepairFill.jsx
+++ components/RepairFill.jsx
@@ -45,7 +45,7 @@ export default function RepairFill() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <img
               className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
-              src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+              src="/Flowers_Lo-Res-8319.jpg"
               alt=""
             />
             <div className="relative" aria-hidden="true">
--- components/ServicesTripleBox.jsx
+++ components/ServicesTripleBox.jsx
@@ -5,32 +5,47 @@ const ServicesTripleBox =()=> {
             <h1 className="text-6xl">Our Services</h1>  
             <p className="text-3xl">In addition to our blooms, we offer a wide range of services.</p>
             <div className="grid lg:grid-cols-3 gap-24">
-            <div className="services-triple-box-item flex flex-col justify-center items-center">
+            <div className="services-triple-box-item flex flex-col justify-center items-center my-12 mb-12">
                 <Image
-                    src="/Flowers_Lo-Res-8305.jpg"
+                    src="/trees7.jpeg"
                     alt="Landscaping"
                     width={300}
                     height={300}/>
                 <h2 className="text-4xl">Tree Planting</h2>
-                <p>Service 1 description</p>
+                <button
+        type="button"
+        className=" my-2 rounded-md bg-sky-600 px-3.5 py-2.5 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Learn more
+      </button>
             </div>
             <div className="services-triple-box-item flex flex-col justify-center items-center">
             <Image
                     src="/Flowers_Lo-Res-8305.jpg"
                     alt="Landscaping"
-                    width={300}
-                    height={300}/>
+                    width={270}
+                    height={270}/>
                 <h2 className="text-4xl">Landscaping</h2>
-                <p>Service 2 description</p>
+                <button
+        type="button"
+        className=" my-2 rounded-md bg-sky-600 px-3.5 py-2.5 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Learn more
+      </button>
             </div>
             <div className="services-triple-box-item flex flex-col justify-center items-center">
             <Image
-                    src="/Flowers_Lo-Res-8305.jpg"
+                    src="/repair7.jpeg"
                     alt="Landscaping"
                     width={300}
                     height={300}/>
                 <h2 className="text-4xl">Home Repair</h2>
-                <p>Service 3 description</p>
+                <button
+        type="button"
+        className=" my-2 rounded-md bg-sky-600 px-3.5 py-2.5 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Learn more
+      </button>
             </div>
             </div>
             </div>
