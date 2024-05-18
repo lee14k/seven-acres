b8545285d11b4e2748b43d71432a4d39c5224fdb
lee14k <kmkovach@mtu.edu>
2024-05-18 09:41:59 -0400

    try to fix

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Switch } from '@headlessui/react'
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+import {
+  BuildingOffice2Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function ContactForm() {
   const [isModalOpen, setIsModalOpen] = useState(false); 
@@ -23,6 +23,13 @@ export default function ContactForm() {
       phoneNumber: formData.get('phoneNumber'),
       message: formData.get('message'),
     };
+    console.log({
+      firstName: formData.get('firstName'),
+      email: formData.get('email'),
+      phoneNumber: formData.get('phoneNumber'),
+      message: formData.get('message'),
+    });
+    
     try {
       const response = await fetch("/api/sendEmail", {
         method: "POST",
@@ -45,84 +52,93 @@ export default function ContactForm() {
       const closeModal = () => {
      setIsModalOpen(false);
    };
-  const [agreed, setAgreed] = useState(false)
-
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-    
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact us</h2>
-    
-      </div>
-      <form onSubmit={handleSubmit} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div className=" gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
-            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+    <div className="relative isolate bridal-one fancy-font pb-12">
+      <div className="flex flex-col justify-center items-center mt-16 text-6xl">
+    <h1>Contact Us</h1>
+    </div>
+        <form onSubmit={handleSubmit}
+        method="POST" className=" pt-12 ">
+          <div className="px-16">
+            <div className="flex flex-col gap-x-8 gap-y-6">
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-semibold leading-6 "
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phoneNumber"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Phone number
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    id="phoneNumber"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-       
-          <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+            <div className="mt-8 flex justify-end">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
+              >
+                Send message
+              </button>
             </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
-              Phone number
-            </label>
-            <div className="relative mt-2.5">
-             
-              <input
-                type="tel"
-                name="phoneNumber"
-                id="phoneNumber"
-                autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={''}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Let's talk
-          </button>
-          {isModalOpen && (
+            {isModalOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center">
           <div className="modal-bg fixed inset-0 bg-black opacity-50"></div>
           <div className="modal-content bg-white p-4 rounded-lg shadow-lg z-50">
@@ -130,15 +146,15 @@ export default function ContactForm() {
             <p>Your submission was successful. Thank you!</p>
             <button
               onClick={closeModal}
-              className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500"
+              className="mt-4 bg-indigo-600 text-white py-2 px-4 rounded hover:bg-red-500"
             >
               Close
             </button>
           </div>
         </div>
       )}
-        </div>
-      </form>
-    </div>
-  )
+          </div>
+        </form>
+      </div>
+  );
 }
