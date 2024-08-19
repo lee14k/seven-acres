d6a7bec5bad08468a881ac156f6717b7359808e4
lee14k <kmkovach@mtu.edu>
2024-08-19 01:00:57 -0400

    adding title metadata

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+      <Html lang="en">
+          <Head/>
+          <title>Seven Acre Garden and Gifts</title>
+          <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+          <body>
+          <Main/>
+          <NextScript/>
+          </body>
+      </Html>
   );
 }
